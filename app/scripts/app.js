7fcf4e8813dd92b921da655e56c1bd75daaa928e
Alicia P <pericia@gmail.com>
2015-08-17 15:29:36 -0400

    First commit: add title

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name mySaladApp
+ * @description
+ * # mySaladApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('mySaladApp', [
+    'ngResource'
+  ]);
