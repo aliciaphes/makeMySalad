49f95a51521da1f8524f5b2860ccfdc0dc8ad1ed
Alicia P <pericia@gmail.com>
2015-08-20 19:35:46 -0400

    Add button to generate random number

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,16 @@
  * Controller of the mySaladApp
  */
 angular.module('mySaladApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.generateNumber = function(){
+    	$scope.randomNumber = Math.floor(Math.random()*4);
+    };
+   
+
   });
